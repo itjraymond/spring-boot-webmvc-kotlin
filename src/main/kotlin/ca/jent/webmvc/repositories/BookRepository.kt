5f1f6924de9bd1aeb7075e1187dfb5788bada49c
Jocelyn Raymond <it.jraymond@gmail.com>
2019-01-23 11:55:32 -0700

    added neat way to create mock data store

--- src/main/kotlin/ca/jent/webmvc/repositories/BookRepository.kt
+++ src/main/kotlin/ca/jent/webmvc/repositories/BookRepository.kt
@@ -3,6 +3,7 @@ package ca.jent.webmvc.repositories
 import ca.jent.webmvc.domain.Book
 import org.springframework.stereotype.Repository
 import java.time.Instant
+import java.util.concurrent.ConcurrentHashMap
 
 @Repository
 class BookRepository {
@@ -13,6 +14,28 @@ class BookRepository {
             "3" to Book("3", "Title three", "John Three", Instant.now()),
             "4" to Book("4", "Title four", "John Four", Instant.now())
     )
+
+    // *****************************************************************************************
+    // NOTE:  another neat way of creating a small data store is as follow:
+    val altBookStore = arrayOf(
+            Book("10", "Title ten", "John Ten", Instant.now()),
+            Book("11", "Title eleven", "John Eleven", Instant.now()),
+            Book("12", "Title twelve", "John Twelve", Instant.now())
+    )
+    val altMapBookStore = ConcurrentHashMap<String, Book>(altBookStore.associateBy(Book::id))
+
+    // NOTE: to improve on the above (because the above actually "expose" the var bookStore or altBookStore
+    //       we can create a companion object instead which will keep encapsulated our data store
+    companion object {
+        val privAltBookStore = arrayOf(
+            Book("10", "Title ten", "John Ten", Instant.now()),
+            Book("11", "Title eleven", "John Eleven", Instant.now()),
+            Book("12", "Title twelve", "John Twelve", Instant.now())
+        )
+        val privMapBookStore = ConcurrentHashMap<String, Book>(privAltBookStore.associateBy(Book::id))
+    }
+    // *****************************************************************************************
+
 
     fun findBook(id: String): Book? = bookStore[id]
 
